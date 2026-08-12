1124612d300f05e41f2898287b94ef38fa3293c4
Guillermo Jabase <guillejabase@gmail.com>
2026-08-11 23:52:47 -0300

    more & better interaction options

--- src/structures/Interaction.ts
+++ src/structures/Interaction.ts
@@ -3,8 +3,8 @@ import { InteractionResponseType, MessageFlags, type APIChatInputApplicationComm
 import { type Client } from './Client';
 import { type Guild } from './Guild';
 import { type GuildMember } from './GuildMember';
-import { type User } from './User';
 
+import { type GuildChannel } from '../util/ChannelFactory';
 import { InteractionOptions } from '../util/InteractionOptions';
 import { Snowflake } from '../util/Snowflake';
 
@@ -14,15 +14,15 @@ export interface ReplyOptions {
 }
 
 export class Interaction {
+  private channelId: string;
   private guildId: string;
+  private memberId: string;
 
   public commandName: string;
   public createdTimestamp: number;
   public id: string;
-  public member: GuildMember;
   public options: InteractionOptions;
   public token: string;
-  public user: User;
 
   public deferred = false;
   public replied = false;
@@ -32,12 +32,13 @@ export class Interaction {
       throw new Error('Gogecord only supports Guild interactions.');
     }
 
+    this.channelId = data.channel.id;
     this.guildId = data.guild_id;
+    this.memberId = data.member.user.id;
 
     this.commandName = data.data.name;
     this.createdTimestamp = new Snowflake(data.id).timestamp;
     this.id = data.id;
-    this.options = new InteractionOptions(data.data.options);
     this.token = data.token;
 
     const guild = this.client.guilds.get(this.guildId);
@@ -46,16 +47,18 @@ export class Interaction {
       throw new Error(`Guild ${this.guildId} was not found in cache for interaction ${this.id}.`);
     }
 
-    const member = guild.members.get(data.member.user.id);
-
-    if (!member) {
-      throw new Error(`Member ${data.member.user.id} was not found in cache for interaction ${this.id}.`);
-    }
-
-    this.member = member;
-    this.user = member.user;
+    this.options = new InteractionOptions(guild, data.data.options);
   }
 
+  public get channel(): GuildChannel {
+    const channel = this.guild.channels.get(this.channelId);
+
+    if (!channel) {
+      throw new Error(`Channel ${this.channelId} was not found in cache.`);
+    }
+
+    return channel;
+  }
   public get guild(): Guild {
     const guild = this.client.guilds.get(this.guildId);
 
@@ -65,24 +68,43 @@ export class Interaction {
 
     return guild;
   }
+  public get member(): GuildMember {
+    const member = this.guild.members.get(this.memberId);
+
+    if (!member) {
+      throw new Error(`Member ${this.memberId} was not found in cache.`);
+    }
+
+    return member;
+  }
 
   public async deferReply(options?: Omit<ReplyOptions, 'content'>): Promise<void> {
     if (this.replied || this.deferred) {
       throw new Error('Interaction has already been replied or deferred.');
     }
 
-    const flags = options?.ephemeral ? MessageFlags.Ephemeral : undefined;
-
     await this.client.request({
       method: 'POST',
       endpoint: `/interactions/${this.id}/${this.token}/callback`,
       body: {
         type: InteractionResponseType.DeferredChannelMessageWithSource,
-        data: { flags }
+        data: {
+          flags: options?.ephemeral ? MessageFlags.Ephemeral : undefined
+        }
       }
     });
 
     this.deferred = true;
+  }
+  public async deleteReply(): Promise<void> {
+    if (!this.replied && !this.deferred) {
+      throw new Error('Cannot delete a reply before replying or deferring.');
+    }
+
+    await this.client.request({
+      method: 'DELETE',
+      endpoint: `/webhooks/${this.client.user.id}/${this.token}/messages/@original`
+    });
   }
   public async editReply(options: ReplyOptions): Promise<void> {
     if (!this.replied && !this.deferred) {
@@ -97,12 +119,24 @@ export class Interaction {
       }
     });
   }
+  public async followUp(options: ReplyOptions): Promise<void> {
+    if (!this.replied && !this.deferred) {
+      throw new Error('Cannot send a follow-up before replying or deferring.');
+    }
+
+    await this.client.request({
+      method: 'POST',
+      endpoint: `/webhooks/${this.client.user.id}/${this.token}`,
+      body: {
+        content: options.content,
+        flags: options.ephemeral ? MessageFlags.Ephemeral : undefined
+      }
+    });
+  }
   public async reply(options: ReplyOptions): Promise<void> {
     if (this.replied || this.deferred) {
       throw new Error('Interaction has already been replied or deferred.');
     }
-
-    const flags = options.ephemeral ? MessageFlags.Ephemeral : undefined;
 
     await this.client.request({
       method: 'POST',
@@ -111,7 +145,7 @@ export class Interaction {
         type: InteractionResponseType.ChannelMessageWithSource,
         data: {
           content: options.content,
-          flags
+          flags: options.ephemeral ? MessageFlags.Ephemeral : undefined
         }
       }
     });
--- src/util/InteractionOptions.ts
+++ src/util/InteractionOptions.ts
@@ -1,45 +1,91 @@
 import { ApplicationCommandOptionType, type APIApplicationCommandInteractionDataOption } from 'discord-api-types/v10';
 
+import { type Guild } from '../structures/Guild';
+import { type GuildMember } from '../structures/GuildMember';
+import { type GuildRole } from '../structures/GuildRole';
+import { type User } from '../structures/User';
+
+import { type GuildChannel } from './ChannelFactory';
+
 export class InteractionOptions {
   private options: Map<string, APIApplicationCommandInteractionDataOption>;
 
-  public constructor(options?: APIApplicationCommandInteractionDataOption[]) {
+  public constructor(private guild: Guild, options?: APIApplicationCommandInteractionDataOption[]) {
     this.options = new Map();
-    options?.forEach((opt) => this.options.set(opt.name, opt));
+
+    this.resolve(options)?.forEach((o) => this.options.set(o.name, o));
+  }
+
+  private resolve(options?: APIApplicationCommandInteractionDataOption[]): APIApplicationCommandInteractionDataOption[] | undefined {
+    if (!options) {
+      return undefined;
+    }
+
+    const first = options[0];
+
+    if (!first) {
+      return options;
+    }
+    if (first.type === ApplicationCommandOptionType.SubcommandGroup && 'options' in first) {
+      return this.resolve(first.options);
+    }
+    if (first.type === ApplicationCommandOptionType.Subcommand && 'options' in first) {
+      return first.options;
+    }
+
+    return options;
   }
 
   public getBoolean(name: string): boolean | null {
-    const opt = this.options.get(name);
-    return opt && opt.type === ApplicationCommandOptionType.Boolean ? (opt.value as boolean) : null;
+    const options = this.options.get(name);
+    return options && options.type === ApplicationCommandOptionType.Boolean ? (options.value as boolean) : null;
   }
+  public getChannel(name: string): GuildChannel | null {
+    const options = this.options.get(name);
 
-  public getChannel(name: string): string | null {
-    const opt = this.options.get(name);
-    return opt && opt.type === ApplicationCommandOptionType.Channel ? (opt.value as string) : null;
+    if (!options || options.type !== ApplicationCommandOptionType.Channel) {
+      return null;
+    }
+
+    return this.guild.channels.get(options.value as string) ?? null;
   }
-
   public getInteger(name: string): number | null {
-    const opt = this.options.get(name);
-    return opt && opt.type === ApplicationCommandOptionType.Integer ? (opt.value as number) : null;
+    const options = this.options.get(name);
+    return options && options.type === ApplicationCommandOptionType.Integer ? (options.value as number) : null;
   }
+  public getMember(name: string): GuildMember | null {
+    const options = this.options.get(name);
 
+    if (!options || options.type !== ApplicationCommandOptionType.User) {
+      return null;
+    }
+
+    return this.guild.members.get(options.value as string) ?? null;
+  }
   public getNumber(name: string): number | null {
-    const opt = this.options.get(name);
-    return opt && opt.type === ApplicationCommandOptionType.Number ? (opt.value as number) : null;
+    const options = this.options.get(name);
+    return options && options.type === ApplicationCommandOptionType.Number ? (options.value as number) : null;
   }
+  public getRole(name: string): GuildRole | null {
+    const options = this.options.get(name);
 
-  public getRole(name: string): string | null {
-    const opt = this.options.get(name);
-    return opt && opt.type === ApplicationCommandOptionType.Role ? (opt.value as string) : null;
+    if (!options || options.type !== ApplicationCommandOptionType.Role) {
+      return null;
+    }
+
+    return this.guild.roles.get(options.value as string) ?? null;
   }
-
   public getString(name: string): string | null {
-    const opt = this.options.get(name);
-    return opt && opt.type === ApplicationCommandOptionType.String ? (opt.value as string) : null;
+    const options = this.options.get(name);
+    return options && options.type === ApplicationCommandOptionType.String ? (options.value as string) : null;
   }
+  public getUser(name: string): User | null {
+    const options = this.options.get(name);
 
-  public getUser(name: string): string | null {
-    const opt = this.options.get(name);
-    return opt && opt.type === ApplicationCommandOptionType.User ? (opt.value as string) : null;
+    if (!options || options.type !== ApplicationCommandOptionType.User) {
+      return null;
+    }
+
+    return this.guild.members.get(options.value as string)?.user ?? this.guild.client.users.get(options.value as string) ?? null;
   }
 }
